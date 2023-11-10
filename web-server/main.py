f20d99257eb77f18b3ed3b4b67d5dbfeb1da6bdd
Luis A. Villalba <luisvillalba_12@hotmail.com>
2023-11-09 21:48:11 -0500

    Se actualizo el archivo main.py

--- web-server/main.py
+++ web-server/main.py
@@ -1,4 +1,26 @@
 import store
+from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
+
+app = FastAPI()
+
+@app.get('/')
+def get_list():
+    return [1,2,3,4,5,6]
+
+@app.get("/contact/", response_class=HTMLResponse)
+async def read_items():
+      return """
+    <html>
+        <head>
+            <title>Some HTML in here</title>
+        </head>
+        <body>
+            <h1>Look ma! HTML!</h1>
+            <p>¡Y yo soy un parrafo!</p>
+        </body>
+    </html>
+    """
 
 def run():
     store.get_categories()
